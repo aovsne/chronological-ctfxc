de8bc1103c28c6c2c3ad2f00783b56328f5edfbb
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 16:20:49 -0400

    update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 (function(angular) {
   'use strict';
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngResource']);
 
-app.controller('Ctrl', ['$scope','$http' function($scope,$http) {
+app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,$http) {
   $http.get('https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyC-JkmgW8D2MB3Iqc176RJz4qO7WMa5lew%20&part=snippet,statistics&fields=items(id,snippet,statistics)')
   .success(function(res){console.log(res)});
 }]);
