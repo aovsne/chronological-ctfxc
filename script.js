dd3feb6d8be57cbd8d54a0c8ddf043936236e329
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 15:58:35 -0400

    initial

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
   'use strict';
 var app = angular.module('app', []);
 
-app.controller('Ctrl', ['$scope', function($scope) {
-    $scope.message = 'very';
+app.controller('Ctrl', ['$scope','$http' function($scope,$http) {
+  $http.get('https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyC-JkmgW8D2MB3Iqc176RJz4qO7WMa5lew%20&part=snippet,statistics&fields=items(id,snippet,statistics)')
+  .success(function(res){console.log(res)});
 }]);
 })(window.angular);
