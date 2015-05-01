42604a897b13722faaf552843050ba7d21629683
Zane Hitchcox <zwhitchcox@gmail.com>
2015-05-01 01:29:09 -0400

    changes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 (function(angular) {
   'use strict';
-var app = angular.module('app', ['ngResource']);
+var app = angular.module('app', []);
 
-app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,$http) {
-  $scope.getVids = function() {
-    $http.get('https://www.googleapis.com/youtube/v3/channels?key=AIzaSyDAoUvvtnXog6O4IoxcUXTG6vHSB9fyaxM&forUsername=ctfxc&part=id')
+app.controller('Ctrl', ['$scope','$http', function($scope,$http) {
+  $scope.getVids = function(uri) {
+    $http.get('https://www.googleapis.com/youtube/v3/search?publishedAfter=2015-01-01T05%3A17%3A02.102Z&part=id&channelId=UCvphW8g3rf4m8LnOarxpU1A&publishedBefore=2015-01-05T05%3A17%3A02.102Z&maxResults=1000&key=AIzaSyDAoUvvtnXog6O4IoxcUXTG6vHSB9fyaxM')
     .success(function(res){console.log(res)});
   }
+
 
 }]);
 })(window.angular);
