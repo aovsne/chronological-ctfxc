ba7e4e8fa6743d8392a45b63d371924f7e361747
Zane Hitchcox <zwhitchcox@gmail.com>
2015-05-01 00:57:03 -0400

    test

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var app = angular.module('app', ['ngResource']);
 
 app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,$http) {
   $scope.getVids = function() {
-    $http.get('https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyC-JkmgW8D2MB3Iqc176RJz4qO7WMa5lew%20&part=snippet,statistics&fields=items(id,snippet,statistics)')
+    $http.get('https://www.googleapis.com/youtube/v3/channels?key=AIzaSyDAoUvvtnXog6O4IoxcUXTG6vHSB9fyaxM&forUsername=ctfxc&part=id')
     .success(function(res){console.log(res)});
   }
 
