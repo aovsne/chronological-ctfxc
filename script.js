6b804e7fb5875f6b031b6ec7abd12f638351f2c8
Zane Hitchcox <zwhitchcox@gmail.com>
2015-05-01 06:58:20 -0400

    changes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
     } else {
       $scope.curStart = Date.parse($scope.st).getTime() + 24*60*60*1000
     }
-    $('#vidList').css('height', window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .8 : .5) +'px')
+    $('#vidList').css('height', window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .8 : .4) +'px')
     var end = new Date ($scope.curStart + 24*60*60*50*1000).toISOString() // limit is 50 per query
     $http.get('https://www.googleapis.com/youtube/v3/search?order=date&publishedAfter='+new Date($scope.curStart).toISOString()+
     '&publishedBefore='+end+'&part=snippet&channelId=UCvphW8g3rf4m8LnOarxpU1A&publish'+
@@ -26,8 +26,8 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
       },[]).reverse()
       $('#stage').append($compile("<div id='ytplayer'></div><br><button class='btn' ng-click='next()'><i class='glyphicon glyphicon-forward'></i></button>")($scope))
       player = new YT.Player('ytplayer', {
-        height: window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .92 : .5),
-        width: window.innerWidth * ((window.innerWidth<768) ? .92 : .),
+        height: window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .92 : .4),
+        width: window.innerWidth * ((window.innerWidth<768) ? .92 : .4),
         videoId: $scope.vids[0].id,
         events: {
           'onReady': onPlayerReady,
@@ -61,8 +61,8 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
         }
       }
       window.onresize = function() {
-        $('#vidList').css('height', window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .8 : .5)+'px')
-        player.setSize(window.innerWidth * ((window.innerWidth<768) ? .92 : .5),window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .92 : .5))
+        $('#vidList').css('height', window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .8 : .4)+'px')
+        player.setSize(window.innerWidth * ((window.innerWidth<768) ? .92 : .4),window.innerWidth * 0.609375 * ((window.innerWidth<768) ? .92 : .4))
         $scope.apply()
       }
     })
