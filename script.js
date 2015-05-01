c4601902b65095bff9000895f8d7acd0a4bd9726
Zane Hitchcox <zwhitchcox@gmail.com>
2015-05-01 05:49:52 -0400

    changes

--- script.js
+++ script.js
@@ -13,10 +13,9 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
     } else {
       $scope.curStart = $scope.curStart.getTime() + 24*60*60*1000
     }
-    console.log($scope.curStart)
     $('#vidList').css('height', window.innerWidth * 0.609375 * .5+'px')
     var end = new Date (Date.parse($scope.curStart).getTime() + 24*60*60*50*1000).toISOString() // limit is 50 per query
-    $http.get('https://www.googleapis.com/youtube/v3/search?order=date&publishedAfter='+$scope.curStart+
+    $http.get('https://www.googleapis.com/youtube/v3/search?order=date&publishedAfter='+$scope.curStart.toISOString()+
     '&publishedBefore='+end+'&part=snippet&channelId=UCvphW8g3rf4m8LnOarxpU1A&publish'+
     'edBefore=2015-01-05T05%3A17%3A02.102Z&maxResults=50&key=AIzaSyDAoUvvtnXog6O4IoxcUXTG6vHSB9fyaxM')
     .success(function(res){
