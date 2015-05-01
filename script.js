04d0908b1d92b122ab54caa99104175b8125ca67
Zane Hitchcox <zwhitchcox@gmail.com>
2015-05-01 05:53:41 -0400

    changes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
     if (start !== undefined) {
       $scope.curStart = new Date (Date.parse($scope.st).getTime() + 24*60*60*1000)
     } else {
-      $scope.curStart = $scope.curStart.getTime() + 24*60*60*1000
+      $scope.curStart = Date($scope.curStart.getTime())
     }
     $('#vidList').css('height', window.innerWidth * 0.609375 * .5+'px')
     var end = new Date (Date.parse($scope.curStart).getTime() + 24*60*60*50*1000).toISOString() // limit is 50 per query
@@ -50,7 +50,7 @@ app.controller('Ctrl', ['$scope','$http','$compile', function($scope,$http,$comp
           if ($scope.playedNext !== id) {
             $scope.$apply(++$scope.curIdx)
             if ($scope.vids[$scope.curIdx]===undefined) {
-              $scope.go($scope.curStart+24*60*60*1000)
+              $scope.go($scope.curStart+50*24*60*60*1000)
             }
             player.loadVideoById($scope.vids[$scope.curIdx].id)
             $scope.$apply()
